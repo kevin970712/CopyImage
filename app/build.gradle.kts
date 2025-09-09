a641cf9c7cbf6b25e94b27ea86a2ae6ff3d5b590
qianxu <qianxuuuu@qq.com>
2025-09-10 02:48:02 +0800

    更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "com.qianxu.copyimage"
     minSdk = 30
     targetSdk = 36
-    versionCode = 4
-    versionName = "2.0.0"
+    versionCode = 5
+    versionName = "2.1.0"
   }
 
   buildTypes {
